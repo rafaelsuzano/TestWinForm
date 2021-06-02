f29edb8fa06b622adb619eaada07ff750d909f79
Rafael Suzano <rafaelsuzano@hotmail.com>
2021-06-02 18:36:58 -0300

    tudo

---
+++ Automacao/Automacao/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Automacao
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton.Checked == true)
+                {
+                MessageBox.Show("Clicou no radio button","Teste");
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("oi");
+        }
+    }
+}
