aa73f1d493ef1be1576305827d43004d276b49ed
Rafael Suzano <rafaelsuzano@hotmail.com>
2021-06-02 18:54:30 -0300

    teste

---
+++ Automacao/AutomacaoTela/Cenarios.cs
@@ -0,0 +1,55 @@
+﻿
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Appium.Windows;
+using System.Threading;
+using System;
+using ExecutarTest;
+
+namespace ExecutarTests
+{
+    [TestClass]
+    public class ScenarioStandard : Session
+    {
+     
+
+        [TestMethod]
+        public void PreencheCampo()
+        {
+            
+            session.FindElementByClassName("WindowsForms10.EDIT.app.0.141b42a_r7_ad1").SendKeys("rafael");
+
+        }
+
+        [TestMethod]
+        public void TesteRadioButton()
+        {
+         
+            session.FindElementByClassName("WindowsForms10.BUTTON.app.0.141b42a_r7_ad1").Click();
+            session.FindElementByClassName("Button").Click();
+        }
+
+
+        [ClassInitialize]
+        public static void ClassInitialize(TestContext context)
+        {
+            
+            Setup(context);
+
+      
+        }
+
+        [ClassCleanup]
+        public static void ClassCleanup()
+        {
+            TearDown();
+        }
+
+        [TestInitialize]
+        public void Clear()
+        {
+         
+        }
+
+      
+    }
+}
---
+++ Automacao/AutomacaoTela/Session.cs
@@ -0,0 +1,47 @@
+﻿
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Appium.Windows;
+using OpenQA.Selenium.Remote;
+using System;
+using System.Diagnostics;
+
+namespace ExecutarTest
+{
+    public class  Session
+    {
+  
+        private const string WindowsApplicationDriverUrl = "http://127.0.0.1:4723";
+        private const string AppId = "C:\\Users\\rafae\\source\\repos\\Automacao\\Automacao\\bin\\Debug\\Automacao.exe";
+
+       
+        private const string WinAppiumExe = "C:\\Users\\rafae\\source\\repos\\Automacao\\tools\\Windows Application Driver\\WinAppDriver.exe";
+
+        protected static WindowsDriver<WindowsElement> session;
+
+        public static void Setup(TestContext context)
+        {
+            if (session == null)
+            {
+
+                Process.Start(WinAppiumExe);
+                DesiredCapabilities appCapabilities = new DesiredCapabilities();
+                appCapabilities.SetCapability("app", AppId);
+                appCapabilities.SetCapability("deviceName", "WindowsPC");
+                session = new WindowsDriver<WindowsElement>(new Uri(WindowsApplicationDriverUrl), appCapabilities);
+              
+                session.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(1.5);
+            }
+        }
+
+        public static void TearDown()
+        {
+
+            if (session != null)
+            { 
+                session.Quit();
+                session = null;
+            }
+        }
+    }
+}
